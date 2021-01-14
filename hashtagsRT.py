963ede970ab4f6a6796f7187018d7c6eb8a7b03b
FernanSLN <61317742+FernanSLN@users.noreply.github.com>
2021-01-14 10:54:04 +0100

    Add files via upload

---
+++ hashtagsRT.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import re
+import numpy as np
+from webweb import Web
+import hashlib
+import networkx as nx
+import matplotlib.pyplot as plt
+import  utils
+
+
+listHashtagsRT2 = utils.get_hashtagsRT2("/home/fernando/Documentos/Lynguo_def2.csv")
+edges2 = utils.get_edgesHashRT2(listHashtagsRT2)
+
+G = nx.Graph()
+G.add_edges_from(edges2)
+subgraphs = utils.get_subgraphs(G)
+subgraphs = [graph for graph in subgraphs if len(graph.nodes) > 5]
+web = Web(title="HashtagsRT", nx_G=subgraphs[0])
+web.display.gravity = 1
+
+name ="graph"
+for i in range(2, len(subgraphs)):
+    web.networks.hashrts.add_layer(nx_G=subgraphs[i])
+
+# show the visualization
+web.show()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import numpy as  np
 
 
 def get_subgraphs(graph):
@@ -30,7 +31,6 @@ def get_retweets(filename):
     # CALCULAR GRAFO RTs
     dfRT = df[['Usuario', 'Texto', 'Fecha']].copy()  # Se copia a un dataframe de trabajo
     idx = dfRT['Texto'].str.contains('RT @', na=False)
-    print(idx)
     dfRT = dfRT[idx]  # Se seleccionan sólo las filas con RT
     subset = dfRT[['Usuario', 'Texto']]  # Se descarta la fecha
     retweetEdges = [list(x) for x in subset.to_numpy()]  # Se transforma en una lista
@@ -47,3 +47,66 @@ def get_edges(values):
             row[1] = matchRT  # Convierte el nombre de la cuenta en hash y lo asigna al elemento
             edges.append(row)
     return edges
+
+
+# Código para hacer gráfica de Hashtags en retuits
+
+def get_hashtagsRT(filename):
+    df = pd.read_csv(filename, sep=';', error_bad_lines=False)
+    stopwords = ['#citizenscience', 'citizenscience', 'rt', 'citizen', 'science', 'citsci', 'cienciaciudadana']
+    dfHashtagsRT = df[['Usuario', 'Texto']].copy()
+    dfHashtagsRT = dfHashtagsRT.drop([78202], axis=0)
+    dfHashtagsRT = dfHashtagsRT.dropna()
+    dfHashtagsRT = dfHashtagsRT[dfHashtagsRT['Texto'].str.match('RT:')]
+    listHashtagsRT = dfHashtagsRT['Texto'].to_numpy()
+    return listHashtagsRT
+
+
+def get_edgesHashRT(values):
+    edges = []
+    for row in values:
+        match = re.findall('#(\w+)', row)
+        for hashtag in match:
+            edges.append(hashtag)
+    return edges
+
+
+def prepare_hashtags(list):
+    stopwords = ['#citizenscience', 'citizenscience', 'rt', 'citizen', 'science', 'citsci', 'cienciaciudadana']
+    list = [x.lower() for x in list]
+    list = [word for word in list if word not in stopwords]
+    list = np.unique(list, return_counts=True)
+    list = sorted((zip(list[1], list[0])), reverse=True)
+    sortedNumberHashtags, sortedHashtagsRT = zip(*list)
+    return sortedNumberHashtags, sortedHashtagsRT
+
+
+# Código para calcular el grafo de Hashtags dentro de los retuits
+
+def get_hashtagsRT2(filename):
+    df = pd.read_csv(filename, sep=';', error_bad_lines=False)
+    df = df.drop([78202], axis=0)
+    stopwords = ['#citizenscience', 'citizenscience', 'rt', 'citizen', 'science', 'citsci', 'cienciaciudadana']
+    dfHashtagsRT = df[['Usuario', 'Texto']]
+    idx = dfHashtagsRT['Texto'].str.match('RT @', na=False)
+    dfHashtagsRT = dfHashtagsRT[idx]
+    listHashtagsRT = [list(x) for x in dfHashtagsRT.to_numpy()]
+    return listHashtagsRT
+
+
+def get_edgesHashRT2(values):
+    edges = []
+    for row in values:
+        match = re.search('#(\w+)', row[1])
+        if match:
+            matchHashRT = match.group(1)
+            row[1] = matchHashRT
+            edges.append(row)
+    return edges
+
+# Combinación de ls ejes de RTs y Citas:
+
+def combined_edges(x,y):
+    combined_edges = x + y
+    return combined_edges
+
