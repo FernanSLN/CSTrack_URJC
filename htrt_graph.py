a48426d214f69699a87b55d48604ce45d1679b4f
davidrol6 <davidrol6@hotmail.com>
2021-01-14 11:47:58 +0100

    Renamed hashtagsrt

--- htrt_graph.py
+++ htrt_graph.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import  utils
 
 
-listHashtagsRT2 = utils.get_hashtagsRT2("/home/fernando/Documentos/Lynguo_def2.csv")
+listHashtagsRT2 = utils.get_hashtagsRT2("sample.csv")
 edges2 = utils.get_edgesHashRT2(listHashtagsRT2)
 
 G = nx.Graph()
 G.add_edges_from(edges2)
 subgraphs = utils.get_subgraphs(G)
 subgraphs = [graph for graph in subgraphs if len(graph.nodes) > 5]
-web = Web(title="HashtagsRT", nx_G=subgraphs[0])
+web = Web(title="HashtagsRT", nx_G=subgraphs[1])
 web.display.gravity = 1
 
 name ="graph"
