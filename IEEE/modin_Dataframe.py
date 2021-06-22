c367e23da771fbf64bda8a8ff31f230b75371685
FernanSLN <fernando.martinezm@urjc.es>
2021-06-22 13:29:35 +0200

    update 22/06/2021

--- IEEE/modin_Dataframe.py
+++ IEEE/modin_Dataframe.py
@@ -1,8 +1,12 @@
 import modin.pandas as pd
 import ray
-# In windows import dask and initiate dask.
 
 ray.init()
+
+# In windows import dask and initiate dask.
+#import os
+#os.environ["MODIN_ENGINE"] = "dask"
+#import modin.pandas as pd
 
 df = pd.read_csv("/home/fernan/Documents/Lynguo_May21.csv", sep=';', encoding='utf-8', decimal=',', error_bad_lines=False)
 
