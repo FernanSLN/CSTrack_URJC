d2830922870934d8eb03250600260b2b1be687e2
FernanSLN <fernando.martinezm@urjc.es>
2021-07-28 14:39:12 +0200

    update 28/07/2021

---
+++ ICALT/keywords_icalt.py
@@ -0,0 +1,83 @@
+k = ["OpenLearning", "openlearning", "educacion", "educación",
+     "#openlearning", "open learning", "learning", "cscl", " tel ", " #tel ",
+     "#learning", "#education", "education", "online courses", "#onlinecourses",
+     "schools", "teacher", " ple ", "#ple", "pedagogy", "#pedagogy", "educational", "#education", " learner ", " teaching ",
+     "inclusive education", "classrooms", "student", "#students", " student ", " chatbot ", " teach ", " learn " ]
+
+k_stop = ["machinelearning"]
+
+keywords = ["education","educational resources","open educational resources","open learning","online courses","open online courses","open learning methodologies","open learning platforms","open learning practices","open linked data",
+            "open access","MOOCs","big data","open education","open flipped classrooms","training","cultural issues","open accreditation","open badges","open virtual mobilities","learning",""]
+
+keywords2 = ["aptel","adaptative and personalised technology enhanced learning", "adaptative and personalised technology-enhanced learning","ple","personal learning enviroments","intelligent learner","group modeling",
+             "adaptive learning methods","adaptive learning techniques","adaptive collaborative learning","mobile learning applications","learning applications","personalized learning applications","adaptive educational games",
+             "educational games","adaptivity in MCOOs","adaptive learning enviroments","practice sharing","experience sharing","learning with adaptive learning enviroments", "teaching with adaptive learning enviroments","teaching analytics",
+            "communities of learners","computing enviroments"]
+
+keywords3 = ["wmute","Wireless, Mobile, Pervasive and Ubiquitous Technologies for Learning","mobile and ubiquitous learning","mobile learning","ubiquitous learning","Multimodal applications","blended learning environment",
+             "Sustainable mobile teaching and learning applications","Sustainable mobile teaching","Sustainable learning applications","Interactive mobile technologies"," wireless communication technologies",
+            "Novel uses of wireless communication technologies","Innovative digital content","evaluation of mobile learning systmes","evaluation of ubiquitous systems","learning approaches"," benefits of mobile technologies in education",
+             "Context-aware learning","Mobile augmented reality tools for learning","Mobile augmented reality tools","Cyber Physical Systems"," Industry 4.0","Sensor-based applications and learning support",
+            "Internet of Things support for learning"]
+
+keywords4 = ["DIGITEL","Digital Game and Intelligent Toy Enhanced Learning","Assessment in Games","Assessment in virtual worlds","Edutainment","educational games","Educational games for people with disabilities","Gamification",
+             "Educational gamification systems","educational robots","educational toys","toys","games","thinking skills","meta-cognitive skills","STE(A)M","mobile games","video games","serious games","simulation","smart city learning",
+            "stealth assess","AI","IoT","computing for learning","learning enviroments"]
+
+keywords5 = ["cscl","computer supported collaborative learning","anecdotes of CSCL","emergency teaching","teaching during the pandemic","teaching during COVID","CSCL usage","results of CSCL","mobile technologies for collaborative learning",
+            "group learning dynamics","learning analytics to support group learning","group learning","cscl perspective","learning communities with CSCL tools","cscl tools","workplace learning with CSCL tools","CSCL scenarios",
+             "interoperability approaches for CSCL tools","Architectures for CSCL tools","HCI Design issues","collaborative applications","Internet of Things in CSCL","Pedagogical agents to support the CSCL",
+            "Computational thinking in CSCL","skills related to CSCL aspects","Peer assessment","Peer reviewing"]
+
+keywords6 = ["BDELA","Big Data in Education and Learning Analytics","Big data theory for learning","big data science for learning","big data technology for learning","science for learning","technology for learning","security of big data",
+            "privacy of big data","big data inclusivity","fairness of big data","ethics of big data","veracity in big data","scalability of machine learning","data mining algorithms for big data","computing infrastructure for big data ",
+            "cloud computing", "grid computing","autonomic computing","stream computing","mobile computing","high performance computing","search in big data","artificial intelligence in big data analytics","uncertainty handling big data",
+            "iot analitycs","big data analytics","Applications of big data in education","Applications of big data in learning analytics"," student’s approach to learning","analytics in academic administration","data-informed learning",
+            "instructional design","gaming analytics","sports analytics","evidence-driven instruction in inter and individual disciplines","analytics in academic strategic planning","cultural analytics","large-scale social networks",
+            "educational data literacy","technological literacy","technological analytics","human literacy","human analytics","Techniques of big data in education","Techniques of big data in knowledge",
+             "Techniques of big data in learning analytics","emerging standards in learning analytics","analysis of unstructured data","analysis of semi-structured data","sentiment analysis","social network analysis",
+             "multimodal learning analytics","large-scale productivity analysis","big data infrastructure for academic institutions","scalable knowledge management","observational research methods for analytics"]
+
+keywords7 = ["TeSTEM","Technology-Enhanced Science, Technology, Engineering and Math Education","Adaptive, Personalized and Context-aware Technology-Enhanced STEM education","STEM education","design with online laboratories",
+             "experiments with online laboratories","social computing","STEM education","agile development","information systems in STEMeducation","wireless technologies for STEM education","mobile technologies or STEM education",
+             "ubiquitous technologies for STEM education","pervasive technologies for STEM education","Technology-Enhanced STEM education","HCI in Technology-Enhanced STEM education",
+             "Participatory Design in technology-enhanced stem education","interaction design in technology-enhanced stem education","disseminate STEM education","learning resources for STEM education","Privacy in STEM platforms",
+             "Learning analytics for STEM","Teacher trainingin STEM education","engagement in STEM education","VR for STEM","AR for STEM","VR","AR"]
+
+keywords8 = ["Technology-Enhanced Language Learning","TELL","TELL theoretical foundations","TELL perspectives","hardware applicable to TELL","software technologies applicable to TELL","AI in language learning",
+            "artificial intelligence in language learning","Virtual Reality platforms","improve student motivation","language lesson design in MOOCs","language assessment in MOOCs","big data in language research",
+             "examples of language learning","e-portfolio","quizzes","infographics","mobile-apps","online interaction to boost language skills","language skills","online interaction","content development for TELL",
+             "content management for TELL","assessment of TELL","evaluation of TELL","PBL","flipped classrooms","reciprocal teaching"]
+
+keywords9 = ["TELEAD","TECHNOLOGY-ENHANCED LEARNING OF DISCIPLINARY PRACTICES","integration of ICT","promote disciplinary practices","development of disciplinary practices"," tools to develop disciplinary practices",
+             "systems to develop disciplinary practices","tools aimed at developing disciplinary practices","teaching-learning strategies","Assessment of disciplinary practices",
+             "innovative methods related to research in technology-enhanced learning of disciplinary practices","Curriculum design for the development of disciplinary practices",
+             "frameworks underpinning technology-enhanced learning of disciplinary practices","models underpinning technology-enhanced learning of disciplinary practices",
+             "theories underpinning technology-enhanced learning of disciplinary practices","techniques for fostering transfer","methods of assessing transfer of disciplinary practices"]
+
+keywords10 = ["TeDISABLE","Technology-supported education for people with disabilities","ICT and inclusive education","ICT","ICT education","inclusive education","Assistive technology applications","Assistive technology learning",
+             "Accessibility of educational content and applications","usability of educational content and applications","Accessibility and usability of educational content and applications","Web2.0","OER","Web2.0 for people with disabilities",
+             "OER for people with disabilities","mobile computing for people with disabilities","cloud computing for people with disabilities","virtual learning enviroments for people with disabilities",
+             "augmented learning enviroments for people with disabilities","robotics for people with disabilities","people with disabilities","ICT use for people with disabilities"]
+
+keywords11 = ["Artificial Intelligence and Smart Learning Environments","AISLE","instructional design","learning paradigms","teaching paradigms","environmental factors","assessment paradigms","social factors","policy",
+             "innovative uses of mature technologies","interactions","adoption","usability","standards","emerging technological paradigms","open educational resources","learning analytics","cloud computing","smart classrooms",
+             "transformation of curriculum","transformation of teaching behaviour","transformation of learning","transformation of administration","transformation of schooling","best practices of infusion","piloting of new ideas",
+             "Fusion of pedagogy/didactics and technology","Fusion of didactics and technology","Fusion of pedagogy and technology",
+              "AI governance and policy for smart learning","AI governance","AI risk management","AI accountability","AI self-surveillance","biases in AI Algorithms","use and misuse of AI","AI on societal impact",
+             "AI technology & practice for smart learning","Explainable AI","interpretable ML","flexibility and contextual understanding by humans","explanation and comprehensible by humans",
+              "intelligent agent assitants","automated conversational robot","Chatbot","AI-enabled personalization"]
+
+keywords12 = ["Augmented Reality and Virtual Worlds in Education and Training","ARVWET","VR pedagogy","VR learning design","AR pedagogy","AR learning design","MR pedagogy","MR learning design","XR pedagogy","XR learning design","VR",
+             "AR","XR","MR","learning design","pedagogy","AR for education","MR for education","XR for education","Virtual worlds","education in virtual worlds","collaborative learning","virtual world for collaborative training",
+              "socio-constructivist","virtual enviroments","learning domains","virtual worlds integrated MOOCs","MOOCs","informal learning","special needs education","cross reality enviroments"]
+
+keywords13 = ["MA-TEL","Motivational and Affective Aspects in Technology-enhanced Learning","Views of motivational and affective theories in technology-enhanced learning",
+              "Impacts of motivational and affective factors on learning with technology","Interactions between cognition and affect in technology-enhance learning",
+              "Individual differences in affective measurements in technology-enhanced learning","Instruments for assessing affective variables in technology-enhanced learning",
+              "Methods for detecting or tracking motivational and affective states in learning process","Affective computing in technology-enhanced learning environments","Modeling, enactment and intelligent use of affective computing",
+              "Motivational and affective regulation in technology-enhanced learning environment","Design of scaffoldings based on motivational and affective evaluation","Design of affective learning companions or emotive agent",
+              "Teachers’ motivational and affective issues on the use of technology-enhanced learning","Designing and revising technology-enhanced learning environments based on learner affect",
+              "Detecting or tracking student visual behaviors in technology-enhanced learning","Human computer interaction in technology-enhanced learning environment",
+              "affective issues in learning with technology","technology enhanced learning","affective computing","modeling enactment","learning process","design of scafoldings","emotive agent",
+             "learner affect","student visual behaviors","human computer interaction"]
